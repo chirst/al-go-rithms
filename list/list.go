27b5c4c0998fcd8913dc404c5b64aeea123064cb
Colton Hirst <colton141@live.com>
2021-08-19 23:45:26 -0600

    use tail

--- list/list.go
+++ list/list.go
@@ -3,6 +3,7 @@ package list
 
 type linkList struct {
 	head *node
+	tail *node
 	len  int
 }
 
@@ -14,6 +15,7 @@ type node struct {
 
 // New returns an instance of a list
 func New() *linkList {
+	// TODO create a list by passing in values
 	return &linkList{}
 }
 
@@ -24,6 +26,7 @@ func (ll *linkList) Len() int {
 
 // Prepend creates a new element at the beginning of the list
 func (ll *linkList) Prepend(value int) {
+	ll.len++
 	if ll.head != nil {
 		oldHead := ll.head
 		ll.head = &node{
@@ -31,33 +34,29 @@ func (ll *linkList) Prepend(value int) {
 			value: value,
 		}
 		ll.head.next.prev = ll.head
-		ll.len++
 		return
 	}
 	ll.head = &node{
 		value: value,
 	}
-	ll.len++
+	ll.tail = ll.head
 }
 
 // Append adds a new element to the end of the list
 func (ll *linkList) Append(value int) {
-	currentNode := ll.head
-	for currentNode != nil {
-		if currentNode.next == nil {
-			currentNode.next = &node{
-				prev:  currentNode,
-				value: value,
-			}
-			ll.len++
-			return
+	ll.len++
+	if ll.head == nil {
+		ll.head = &node{
+			value: value,
 		}
-		currentNode = currentNode.next
+		ll.tail = ll.head
+		return
 	}
-	ll.head = &node{
+	ll.tail.next = &node{
+		prev:  ll.tail,
 		value: value,
 	}
-	ll.len++
+	ll.tail = ll.tail.next
 }
 
 // Shift removes the first element in the list
@@ -65,14 +64,14 @@ func (ll *linkList) Shift() {
 	if ll.head == nil {
 		return
 	}
+	ll.len--
 	if ll.head.next == nil {
 		ll.head = nil
-		ll.len = 0
+		ll.tail = nil
 		return
 	}
 	ll.head.next.prev = nil
 	ll.head = ll.head.next
-	ll.len--
 }
 
 // Pop removes the last element in the list
@@ -80,15 +79,12 @@ func (ll *linkList) Pop() {
 	if ll.head == nil {
 		return
 	}
+	ll.len--
 	if ll.head.next == nil {
 		ll.head = nil
-		ll.len = 0
+		ll.tail = nil
 		return
 	}
-	currentNode := ll.head
-	for currentNode.next != nil {
-		currentNode = currentNode.next
-	}
-	currentNode.prev.next = nil
-	ll.len--
+	ll.tail = ll.tail.prev
+	ll.tail.next = nil
 }
