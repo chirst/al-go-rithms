3fa780b1b132b9e1b8287b4fe45f053158f18480
Colton Hirst <colton141@live.com>
2021-08-21 23:59:55 -0600

    tests way easier to write and read

--- list/list_test.go
+++ list/list_test.go
@@ -6,9 +6,7 @@ import (
 
 func TestNew(t *testing.T) {
 	l := New(1, 2, 3)
-	if l.Len() != 3 {
-		t.Fatalf("expected new to generate 3 values")
-	}
+	checkLen(t, l, 3)
 }
 
 func TestLen(t *testing.T) {
@@ -16,98 +14,60 @@ func TestLen(t *testing.T) {
 	t.Run("prepend", func(t *testing.T) {
 		l := New()
 
-		if len := l.Len(); len != 0 {
-			t.Fatalf("expected len to be 0 got %v", len)
-		}
-
+		checkLen(t, l, 0)
 		l.Prepend(1)
-		if len := l.Len(); len != 1 {
-			t.Fatalf("expected len to be 1 got %v", len)
-		}
-
+		checkLen(t, l, 1)
 		l.Prepend(2)
-		if len := l.Len(); len != 2 {
-			t.Fatalf("expected len to be 2 got %v", len)
-		}
-
+		checkLen(t, l, 2)
 		l.Prepend(3)
-		if len := l.Len(); len != 3 {
-			t.Fatalf("expected len to be 3 got %v", len)
-		}
+		checkLen(t, l, 3)
 	})
 
 	t.Run("append", func(t *testing.T) {
 		l := New()
 
-		if len := l.Len(); len != 0 {
-			t.Fatalf("expected len to be 0 got %v", len)
-		}
-
+		checkLen(t, l, 0)
 		l.Append(1)
-		if len := l.Len(); len != 1 {
-			t.Fatalf("expected len to be 1 got %v", len)
-		}
-
+		checkLen(t, l, 1)
 		l.Append(2)
-		if len := l.Len(); len != 2 {
-			t.Fatalf("expected len to be 2 got %v", len)
-		}
-
+		checkLen(t, l, 2)
 		l.Append(3)
-		if len := l.Len(); len != 3 {
-			t.Fatalf("expected len to be 3 got %v", len)
-		}
+		checkLen(t, l, 3)
 	})
 
 	t.Run("shift", func(t *testing.T) {
-		l := New()
+		l := New(1, 2, 3)
 
-		l.Append(1)
-		l.Append(2)
-		l.Append(3)
-
+		checkLen(t, l, 3)
 		l.Shift()
-		if len := l.Len(); len != 2 {
-			t.Fatalf("expected len to be 2 got %v", len)
-		}
+		checkLen(t, l, 2)
 		l.Shift()
-		if len := l.Len(); len != 1 {
-			t.Fatalf("expected len to be 1 got %v", len)
-		}
+		checkLen(t, l, 1)
 		l.Shift()
-		if len := l.Len(); len != 0 {
-			t.Fatalf("expected len to be 0 got %v", len)
-		}
+		checkLen(t, l, 0)
 		l.Shift()
-		if len := l.Len(); len != 0 {
-			t.Fatalf("expected len to be 0 got %v", len)
-		}
+		checkLen(t, l, 0)
 	})
 
 	t.Run("pop", func(t *testing.T) {
-		l := New()
+		l := New(1, 2, 3)
 
-		l.Append(1)
-		l.Append(2)
-		l.Append(3)
-
+		checkLen(t, l, 3)
 		l.Pop()
-		if len := l.Len(); len != 2 {
-			t.Fatalf("expected len to be 2 got %v", len)
-		}
+		checkLen(t, l, 2)
 		l.Pop()
-		if len := l.Len(); len != 1 {
-			t.Fatalf("expected len to be 1 got %v", len)
-		}
+		checkLen(t, l, 1)
 		l.Pop()
-		if len := l.Len(); len != 0 {
-			t.Fatalf("expected len to be 0 got %v", len)
-		}
+		checkLen(t, l, 0)
 		l.Pop()
-		if len := l.Len(); len != 0 {
-			t.Fatalf("expected len to be 0 got %v", len)
-		}
+		checkLen(t, l, 0)
 	})
+}
+
+func checkLen(t *testing.T, l *linkList, expectedLen int) {
+	if len := l.Len(); len != expectedLen {
+		t.Errorf("expected len to be %v got %v", expectedLen, len)
+	}
 }
 
 func TestPrepend(t *testing.T) {
@@ -117,50 +77,18 @@ func TestPrepend(t *testing.T) {
 	l.Prepend(2)
 	l.Prepend(3)
 
-	firstNode := l.getNode(0)
-	secondNode := l.getNode(1)
-	thirdNode := l.getNode(2)
+	checkNodeValue(t, l, 0, 3)
+	checkNodeValue(t, l, 1, 2)
+	checkNodeValue(t, l, 2, 1)
 
-	if firstNode == nil {
-		t.Fatalf("expected first node not to be nil")
-	}
-	if secondNode == nil {
-		t.Fatalf("expected second node not to be nil")
-	}
-	if thirdNode == nil {
-		t.Fatalf("expected third node not to be nil")
-	}
+	checkNodePrev(t, l, 0, nil)
+	checkNodeNext(t, l, 0, l.getNode(1))
 
-	if firstNode.value != 3 {
-		t.Fatalf("expected first node value of 3")
-	}
-	if secondNode.value != 2 {
-		t.Fatalf("expected second node value of 2")
-	}
-	if thirdNode.value != 1 {
-		t.Fatalf("expected third node value of 1")
-	}
+	checkNodePrev(t, l, 1, l.getNode(0))
+	checkNodeNext(t, l, 1, l.getNode(2))
 
-	if firstNode.prev != nil {
-		t.Fatalf("expected first node prev value to be nil")
-	}
-	if firstNode.next != secondNode {
-		t.Fatalf("expected first node next to be second node")
-	}
-
-	if secondNode.prev != firstNode {
-		t.Fatalf("expected second node prev to be first node")
-	}
-	if secondNode.next != thirdNode {
-		t.Fatalf("expected second node next to be third node")
-	}
-
-	if thirdNode.prev != secondNode {
-		t.Fatalf("expected third node prev to be second node")
-	}
-	if thirdNode.next != nil {
-		t.Fatalf("expected third node next to be nil")
-	}
+	checkNodePrev(t, l, 2, l.getNode(1))
+	checkNodeNext(t, l, 2, nil)
 }
 
 func TestAppend(t *testing.T) {
@@ -170,50 +98,18 @@ func TestAppend(t *testing.T) {
 	l.Append(2)
 	l.Append(3)
 
-	firstNode := l.getNode(0)
-	secondNode := l.getNode(1)
-	thirdNode := l.getNode(2)
+	checkNodeValue(t, l, 0, 1)
+	checkNodeValue(t, l, 1, 2)
+	checkNodeValue(t, l, 2, 3)
 
-	if firstNode == nil {
-		t.Fatalf("expected first node not to be nil")
-	}
-	if secondNode == nil {
-		t.Fatalf("expected second node not to be nil")
-	}
-	if thirdNode == nil {
-		t.Fatalf("expected third node not to be nil")
-	}
+	checkNodePrev(t, l, 0, nil)
+	checkNodeNext(t, l, 0, l.getNode(1))
 
-	if firstNode.value != 1 {
-		t.Fatalf("expected first node value of 1")
-	}
-	if secondNode.value != 2 {
-		t.Fatalf("expected second node value of 2")
-	}
-	if thirdNode.value != 3 {
-		t.Fatalf("expected third node value of 3")
-	}
+	checkNodePrev(t, l, 1, l.getNode(0))
+	checkNodeNext(t, l, 1, l.getNode(2))
 
-	if firstNode.prev != nil {
-		t.Fatalf("expected first node prev value to be nil")
-	}
-	if firstNode.next != secondNode {
-		t.Fatalf("expected first node next to be second node")
-	}
-
-	if secondNode.prev != firstNode {
-		t.Fatalf("expected second node prev to be first node")
-	}
-	if secondNode.next != thirdNode {
-		t.Fatalf("expected second node next to be third node")
-	}
-
-	if thirdNode.prev != secondNode {
-		t.Fatalf("expected third node prev to be second node")
-	}
-	if thirdNode.next != nil {
-		t.Fatalf("expected third node next to be nil")
-	}
+	checkNodePrev(t, l, 2, l.getNode(1))
+	checkNodeNext(t, l, 2, nil)
 }
 
 func TestShift(t *testing.T) {
@@ -224,49 +120,19 @@ func TestShift(t *testing.T) {
 	l.Append(3)
 
 	l.Shift()
-	if l.getNode(0) == nil {
-		t.Fatalf("expected first node not to be nil")
-	}
-	if l.getNode(1) == nil {
-		t.Fatalf("expected second node not to be nil")
-	}
-	if l.getNode(2) != nil {
-		t.Fatalf("expected third node to be nil")
-	}
-	if l.getNode(0).value != 2 {
-		t.Fatalf("expected first node value of 2")
-	}
-	if l.getNode(1).value != 3 {
-		t.Fatalf("expected second node value of 3")
-	}
+	checkNodeValue(t, l, 0, 2)
+	checkNodeValue(t, l, 1, 3)
+	checkNodeNil(t, l, 2)
 
 	l.Shift()
-	if l.getNode(0) == nil {
-		t.Fatalf("expected first node not to be nil")
-	}
-	if l.getNode(1) != nil {
-		t.Fatalf("expected second node to be nil")
-	}
-	if l.getNode(2) != nil {
-		t.Fatalf("expected third node to be nil")
-	}
-	if l.getNode(0).value != 3 {
-		t.Fatalf("expected first node value of 3")
-	}
+	checkNodeValue(t, l, 0, 3)
+	checkNodeNil(t, l, 1)
+	checkNodeNil(t, l, 2)
 
 	l.Shift()
-	if l.getNode(0) != nil {
-		t.Fatalf("expected first node to be nil")
-	}
-	if l.getNode(1) != nil {
-		t.Fatalf("expected second node to be nil")
-	}
-	if l.getNode(2) != nil {
-		t.Fatalf("expected third node to be nil")
-	}
-
-	// assert can shift empty list
-	l.Shift()
+	checkNodeNil(t, l, 0)
+	checkNodeNil(t, l, 1)
+	checkNodeNil(t, l, 2)
 }
 
 func TestPop(t *testing.T) {
@@ -277,49 +143,71 @@ func TestPop(t *testing.T) {
 	l.Append(3)
 
 	l.Pop()
-	if l.getNode(0) == nil {
-		t.Fatalf("expected first node not to be nil")
-	}
-	if l.getNode(1) == nil {
-		t.Fatalf("expected second node not to be nil")
-	}
-	if l.getNode(2) != nil {
-		t.Fatalf("expected third node to be nil")
-	}
-	if l.getNode(0).value != 1 {
-		t.Fatalf("expected first node value of 1")
-	}
-	if l.getNode(1).value != 2 {
-		t.Fatalf("expected second node value of 2")
-	}
+	checkNodeValue(t, l, 0, 1)
+	checkNodeValue(t, l, 1, 2)
+	checkNodeNil(t, l, 2)
 
 	l.Pop()
-	if l.getNode(0) == nil {
-		t.Fatalf("expected first node not to be nil")
-	}
-	if l.getNode(1) != nil {
-		t.Fatalf("expected second node to be nil")
-	}
-	if l.getNode(2) != nil {
-		t.Fatalf("expected third node to be nil")
-	}
-	if l.getNode(0).value != 1 {
-		t.Fatalf("expected first node value of 1")
-	}
+	checkNodeValue(t, l, 0, 1)
+	checkNodeNil(t, l, 1)
+	checkNodeNil(t, l, 2)
 
 	l.Pop()
-	if l.getNode(0) != nil {
-		t.Fatalf("expected first node to be nil")
-	}
-	if l.getNode(1) != nil {
-		t.Fatalf("expected second node to be nil")
-	}
-	if l.getNode(2) != nil {
-		t.Fatalf("expected third node to be nil")
-	}
+	checkNodeNil(t, l, 0)
+	checkNodeNil(t, l, 1)
+	checkNodeNil(t, l, 2)
+}
 
-	// assert can pop empty list
-	l.Pop()
+func checkNodeValue(t *testing.T, l *linkList, nodeIndex int, wantValue int) {
+	n := l.getNode(nodeIndex)
+	if n == nil {
+		t.Errorf("expected node value at index: %v, not to be nil", nodeIndex)
+	}
+	if n.value != wantValue {
+		t.Errorf(
+			"expected node value at index: %v to be %v got %v",
+			nodeIndex,
+			wantValue,
+			n.value,
+		)
+	}
+}
+
+func checkNodePrev(t *testing.T, l *linkList, nodeIndex int, wantNode *node) {
+	n := l.getNode(nodeIndex)
+	if n == nil {
+		t.Errorf("expected node value at index: %v, not to be nil", nodeIndex)
+	}
+	if n.prev != wantNode {
+		t.Errorf(
+			"expected node prev at index: %v to be %v got %v",
+			nodeIndex,
+			wantNode,
+			n.prev,
+		)
+	}
+}
+
+func checkNodeNext(t *testing.T, l *linkList, nodeIndex int, wantNode *node) {
+	n := l.getNode(nodeIndex)
+	if n == nil {
+		t.Errorf("expected node value at index: %v, not to be nil", nodeIndex)
+	}
+	if n.next != wantNode {
+		t.Errorf(
+			"expected node next at index: %v to be %v got %v",
+			nodeIndex,
+			wantNode,
+			n.next,
+		)
+	}
+}
+
+func checkNodeNil(t *testing.T, l *linkList, nodeIndex int) {
+	n := l.getNode(nodeIndex)
+	if n != nil {
+		t.Errorf("expected node at index: %v to be nil", nodeIndex)
+	}
 }
 
 func (ll *linkList) getNode(index int) *node {
