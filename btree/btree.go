44a469e7bc952d427978a72fb5a31183cf99179f
Colton Hirst <colton141@live.com>
2021-09-25 22:40:25 -0600

    B-tree (#2)

---
+++ btree/btree.go
@@ -0,0 +1,241 @@
+// Package btree implements a btree that shouldn't be taken too seriously.
+package btree
+
+// TODO:
+// - implement Delete
+// - smaller functions
+
+import (
+	"errors"
+)
+
+type btree struct {
+	root   *node
+	degree int
+}
+
+// node consists of one or many elements and many children and always maintains
+// elements + 1 number of children.
+type node struct {
+	parent   *node
+	elements []int
+	children []*node
+}
+
+// New returns a tree with the given degree.
+//
+// When a node reaches the given degree of elements the node will split.
+//
+// This implementation allows degrees between 3-7 inclusive.
+//
+// Given `values` are provided the tree will be populated with the `values`. The
+// complexity of this is O(n log n).
+func New(degree int, values ...int) (*btree, error) {
+	if degree < 3 {
+		return nil, errors.New("tree must not have degree less than 3")
+	}
+	if 7 < degree {
+		return nil, errors.New("tree must not have degree greater than 7")
+	}
+	nt := &btree{
+		degree: degree,
+	}
+	for _, v := range values {
+		nt.Insert(v)
+	}
+	return nt, nil
+}
+
+// Exists checks for the existense of the given `value`.
+//
+// The complexity is O(log n).
+func (bt *btree) Exists(value int) bool {
+	if bt.root == nil {
+		return false
+	}
+	return bt.exists(bt.root, value)
+}
+
+func (bt *btree) exists(n *node, value int) bool {
+	// Check if value is in the current node.
+	for _, e := range n.elements {
+		if e == value {
+			return true
+		}
+	}
+	// If the node is a leaf the value does not exist
+	if len(n.children) == 0 {
+		return false
+	}
+	// Recursively search next node
+	var childNode *node
+	for i, el := range n.elements {
+		if el > value {
+			childNode = n.children[i]
+			break
+		}
+		if i == len(n.elements)-1 {
+			childNode = n.children[i+1]
+			break
+		}
+	}
+	return bt.exists(childNode, value)
+}
+
+// Insert inserts an element into the tree.
+//
+// Given the value already exists in the tree, the value will still be inserted.
+//
+// The complexity is O(log n).
+func (bt *btree) Insert(value int) {
+	// No nodes at all so create a root node.
+	if bt.root == nil {
+		bt.root = &node{
+			elements: []int{value},
+		}
+		return
+	}
+	// Root node exists, attempt to insert into the root node. In case the root
+	// node is not a leaf, `insert` will recursively find a leaf node to insert
+	// into.
+	bt.insert(bt.root, value)
+}
+
+// insert recursively follows nodes until hitting a leaf node. Once a leaf is
+// hit, an insert will be performed (whether or not the insert is allowed)
+// followed by splitting to ensure the tree is in a valid state.
+func (bt *btree) insert(n *node, value int) {
+	// Insert if leaf.
+	if len(n.children) == 0 {
+		n.elements = append(n.elements, value)
+		// Start splitting if needed.
+		bt.split(n)
+	} else {
+		// Recursively try to insert on the next sub tree.
+		for i, el := range n.elements {
+			if el > value {
+				bt.insert(n.children[i], value)
+				break
+			}
+			if i == len(n.elements)-1 {
+				bt.insert(n.children[i+1], value)
+				break
+			}
+		}
+	}
+}
+
+// split splits a node exceeding a tree's degree. split continues to recursively
+// process parent nodes until all parent nodes have valid degrees.
+func (bt *btree) split(n *node) {
+	// Base case, done splitting.
+	if len(n.elements) < bt.degree {
+		return
+	}
+
+	// `n` is the root node. This means the tree will grow in height.
+	if n.parent == nil {
+		// Gather left, right, and middle partitions of the current node.
+		middleIndex := (len(n.elements) - 1) / 2
+		value := n.elements[middleIndex]
+		lefts := n.elements[:middleIndex]
+		rights := n.elements[middleIndex+1:]
+		leftChildren := []*node{}
+		rightChildren := []*node{}
+		if len(n.children) != 0 {
+			leftChildren = n.children[:middleIndex+1]
+			rightChildren = n.children[middleIndex+1:]
+		}
+
+		// Create a new root node from the middle partition (the middle
+		// partition always being size 1) making the root node's two children
+		// from the left and right partition.
+		bt.root = &node{
+			elements: []int{value},
+		}
+		bt.root.children = append(bt.root.children, &node{
+			parent:   bt.root,
+			elements: lefts,
+			children: leftChildren,
+		})
+		bt.root.children = append(bt.root.children, &node{
+			parent:   bt.root,
+			elements: rights,
+			children: rightChildren,
+		})
+		return
+	} else {
+		// Gather left, right, and middle partitions of the current node.
+		middleIndex := (len(n.elements) - 1) / 2
+		middleElement := n.elements[middleIndex]
+		leftElements := n.elements[:middleIndex]
+		rightElements := n.elements[middleIndex+1:]
+		leftChildren := []*node{}
+		rightChildren := []*node{}
+		if len(n.children) != 0 {
+			rightChildren = n.children[middleIndex+1:]
+			leftChildren = n.children[:middleIndex-1]
+		}
+
+		// Remove old parent/child relation.
+		for i, element := range n.parent.elements {
+			// End of list, remove last child.
+			if element < leftElements[0] && i+1 == len(n.parent.elements) {
+				n.parent.children = n.parent.children[:len(n.parent.children)-1]
+				break
+			}
+			// Gone past by 1.
+			if element > leftElements[0] {
+				n.parent.children = append(n.parent.children[:i], n.parent.children[i+1:]...)
+				break
+			}
+		}
+
+		// Insert middle into parent and put lefts and rights as children.
+		for i, element := range n.parent.elements {
+			// End of list, append to the end.
+			if element < middleElement && i+1 == len(n.parent.elements) {
+				n.parent.elements = append(n.parent.elements, middleElement)
+				newLeft := &node{
+					parent:   n.parent,
+					elements: leftElements,
+					children: leftChildren,
+				}
+				n.parent.children = append(n.parent.children, newLeft)
+				newRight := &node{
+					parent:   n.parent,
+					elements: rightElements,
+					children: rightChildren,
+				}
+				n.parent.children = append(n.parent.children, newRight)
+			}
+			// Gone past by 1.
+			if element > middleElement {
+				// Insert middle into parent elements.
+				leftParentElements := n.parent.elements[:i-1]
+				leftParentElements = append(leftParentElements, middleElement)
+				rightParentElements := n.parent.elements[i:]
+				n.parent.elements = append(leftParentElements, rightParentElements...)
+
+				// Insert lefts and rights into parent children.
+				leftParentChildren := n.parent.children[:i-1]
+				newLeft := &node{
+					parent:   n.parent,
+					elements: leftElements,
+					children: leftChildren,
+				}
+				leftParentChildren = append(leftParentChildren, newLeft)
+				newRight := &node{
+					parent:   n.parent,
+					elements: rightElements,
+					children: rightChildren,
+				}
+				leftParentChildren = append(leftParentChildren, newRight)
+				rightParentChildren := n.parent.children[i:]
+				n.parent.children = append(leftParentChildren, rightParentChildren...)
+			}
+		}
+		// Recursively continue to split.
+		bt.split(n.parent)
+	}
+}
---
+++ btree/btree_test.go
@@ -0,0 +1,502 @@
+package btree
+
+import "testing"
+
+func TestInsertDegree3(t *testing.T) {
+	bt, _ := New(3)
+
+	t.Run("insert 1", func(t *testing.T) {
+		bt.Insert(1)
+		bt.root.checkElements(t, 1)
+		bt.root.checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 2", func(t *testing.T) {
+		bt.Insert(2)
+		bt.root.checkElements(t, 1, 2)
+		bt.root.checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 3", func(t *testing.T) {
+		bt.Insert(3)
+		bt.root.checkElements(t, 2)
+		bt.root.checkChildrenLength(t, 2)
+		bt.root.children[0].checkElements(t, 1)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 3)
+		bt.root.children[1].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 4", func(t *testing.T) {
+		bt.Insert(4)
+		bt.root.checkElements(t, 2)
+		bt.root.checkChildrenLength(t, 2)
+		bt.root.children[0].checkElements(t, 1)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 3, 4)
+		bt.root.children[1].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 5", func(t *testing.T) {
+		bt.Insert(5)
+		bt.root.checkElements(t, 2, 4)
+		bt.root.checkChildrenLength(t, 3)
+		bt.root.children[0].checkElements(t, 1)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 3)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 5)
+		bt.root.children[2].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 6", func(t *testing.T) {
+		bt.Insert(6)
+		bt.root.checkElements(t, 2, 4)
+		bt.root.checkChildrenLength(t, 3)
+		bt.root.children[0].checkElements(t, 1)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 3)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 5, 6)
+		bt.root.children[2].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 7", func(t *testing.T) {
+		bt.Insert(7)
+
+		// top level
+		bt.root.checkElements(t, 4)
+		bt.root.checkChildrenLength(t, 2)
+
+		// second level left to right
+		bt.root.children[0].checkElements(t, 2)
+		bt.root.children[0].checkChildrenLength(t, 2)
+		bt.root.children[0].children[0].checkElements(t, 1)
+		bt.root.children[0].children[0].checkChildrenLength(t, 0)
+		bt.root.children[0].children[1].checkElements(t, 3)
+		bt.root.children[0].children[1].checkChildrenLength(t, 0)
+
+		// third level left to right
+		bt.root.children[1].checkElements(t, 6)
+		bt.root.children[1].checkChildrenLength(t, 2)
+		bt.root.children[1].children[0].checkElements(t, 5)
+		bt.root.children[1].children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].children[1].checkElements(t, 7)
+		bt.root.children[1].children[1].checkChildrenLength(t, 0)
+	})
+}
+
+func TestInsertDegree4(t *testing.T) {
+	bt, _ := New(4)
+
+	t.Run("insert 1", func(t *testing.T) {
+		bt.Insert(1)
+		bt.root.checkElements(t, 1)
+		bt.root.checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 2", func(t *testing.T) {
+		bt.Insert(2)
+		bt.root.checkElements(t, 1, 2)
+		bt.root.checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 3", func(t *testing.T) {
+		bt.Insert(3)
+		bt.root.checkElements(t, 1, 2, 3)
+		bt.root.checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 4", func(t *testing.T) {
+		bt.Insert(4)
+		bt.root.checkElements(t, 2)
+		bt.root.checkChildrenLength(t, 2)
+		bt.root.children[0].checkElements(t, 1)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 3, 4)
+		bt.root.children[1].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 5", func(t *testing.T) {
+		bt.Insert(5)
+		bt.root.checkElements(t, 2)
+		bt.root.checkChildrenLength(t, 2)
+		bt.root.children[0].checkElements(t, 1)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 3, 4, 5)
+		bt.root.children[1].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 6", func(t *testing.T) {
+		bt.Insert(6)
+		bt.root.checkElements(t, 2, 4)
+		bt.root.checkChildrenLength(t, 3)
+		bt.root.children[0].checkElements(t, 1)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 3)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 5, 6)
+		bt.root.children[2].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 7", func(t *testing.T) {
+		bt.Insert(7)
+
+		// top level
+		bt.root.checkElements(t, 2, 4)
+		bt.root.checkChildrenLength(t, 3)
+
+		// second level left to right
+		bt.root.children[0].checkElements(t, 1)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 3)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 5, 6, 7)
+		bt.root.children[2].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 8", func(t *testing.T) {
+		bt.Insert(8)
+
+		// top level
+		bt.root.checkElements(t, 2, 4, 6)
+		bt.root.checkChildrenLength(t, 4)
+
+		// second level left to right
+		bt.root.children[0].checkElements(t, 1)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 3)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 5)
+		bt.root.children[2].checkChildrenLength(t, 0)
+		bt.root.children[3].checkElements(t, 7, 8)
+		bt.root.children[3].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 9", func(t *testing.T) {
+		bt.Insert(9)
+
+		// top level
+		bt.root.checkElements(t, 2, 4, 6)
+		bt.root.checkChildrenLength(t, 4)
+
+		// second level left to right
+		bt.root.children[0].checkElements(t, 1)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 3)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 5)
+		bt.root.children[2].checkChildrenLength(t, 0)
+		bt.root.children[3].checkElements(t, 7, 8, 9)
+		bt.root.children[3].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 10", func(t *testing.T) {
+		bt.Insert(10)
+
+		// top level
+		bt.root.checkElements(t, 4)
+		bt.root.checkChildrenLength(t, 2)
+
+		// second level left to right
+		bt.root.children[0].checkElements(t, 2)
+		bt.root.children[0].checkChildrenLength(t, 2)
+		bt.root.children[1].checkElements(t, 6, 8)
+		bt.root.children[1].checkChildrenLength(t, 3)
+
+		// third level left to right
+		bt.root.children[0].children[0].checkElements(t, 1)
+		bt.root.children[0].children[0].checkChildrenLength(t, 0)
+		bt.root.children[0].children[1].checkElements(t, 3)
+		bt.root.children[0].children[1].checkChildrenLength(t, 0)
+
+		bt.root.children[1].children[0].checkElements(t, 5)
+		bt.root.children[1].children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].children[1].checkElements(t, 7)
+		bt.root.children[1].children[1].checkChildrenLength(t, 0)
+		bt.root.children[1].children[2].checkElements(t, 9, 10)
+		bt.root.children[1].children[2].checkChildrenLength(t, 0)
+	})
+}
+
+func TestInsertDegree5(t *testing.T) {
+	bt, _ := New(5)
+
+	t.Run("insert 1", func(t *testing.T) {
+		bt.Insert(1)
+		bt.root.checkElements(t, 1)
+		bt.root.checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 2", func(t *testing.T) {
+		bt.Insert(2)
+		bt.root.checkElements(t, 1, 2)
+		bt.root.checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 3", func(t *testing.T) {
+		bt.Insert(3)
+		bt.root.checkElements(t, 1, 2, 3)
+		bt.root.checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 4", func(t *testing.T) {
+		bt.Insert(4)
+		bt.root.checkElements(t, 1, 2, 3, 4)
+		bt.root.checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 5", func(t *testing.T) {
+		bt.Insert(5)
+		bt.root.checkElements(t, 3)
+		bt.root.checkChildrenLength(t, 2)
+		bt.root.children[0].checkElements(t, 1, 2)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 4, 5)
+		bt.root.children[1].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 6", func(t *testing.T) {
+		bt.Insert(6)
+		bt.root.checkElements(t, 3)
+		bt.root.checkChildrenLength(t, 2)
+		bt.root.children[0].checkElements(t, 1, 2)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 4, 5, 6)
+		bt.root.children[1].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 7", func(t *testing.T) {
+		bt.Insert(7)
+		bt.root.checkElements(t, 3)
+		bt.root.checkChildrenLength(t, 2)
+		bt.root.children[0].checkElements(t, 1, 2)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 4, 5, 6, 7)
+		bt.root.children[1].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 8", func(t *testing.T) {
+		bt.Insert(8)
+		bt.root.checkElements(t, 3, 6)
+		bt.root.checkChildrenLength(t, 3)
+		bt.root.children[0].checkElements(t, 1, 2)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 4, 5)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 7, 8)
+		bt.root.children[2].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 9", func(t *testing.T) {
+		bt.Insert(9)
+		bt.root.checkElements(t, 3, 6)
+		bt.root.checkChildrenLength(t, 3)
+		bt.root.children[0].checkElements(t, 1, 2)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 4, 5)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 7, 8, 9)
+		bt.root.children[2].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 10", func(t *testing.T) {
+		bt.Insert(10)
+		bt.root.checkElements(t, 3, 6)
+		bt.root.checkChildrenLength(t, 3)
+		bt.root.children[0].checkElements(t, 1, 2)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 4, 5)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 7, 8, 9, 10)
+		bt.root.children[2].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 11", func(t *testing.T) {
+		bt.Insert(11)
+		bt.root.checkElements(t, 3, 6, 9)
+		bt.root.checkChildrenLength(t, 4)
+		bt.root.children[0].checkElements(t, 1, 2)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 4, 5)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 7, 8)
+		bt.root.children[2].checkChildrenLength(t, 0)
+		bt.root.children[3].checkElements(t, 10, 11)
+		bt.root.children[3].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 12", func(t *testing.T) {
+		bt.Insert(12)
+		bt.root.checkElements(t, 3, 6, 9)
+		bt.root.checkChildrenLength(t, 4)
+		bt.root.children[0].checkElements(t, 1, 2)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 4, 5)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 7, 8)
+		bt.root.children[2].checkChildrenLength(t, 0)
+		bt.root.children[3].checkElements(t, 10, 11, 12)
+		bt.root.children[3].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 13", func(t *testing.T) {
+		bt.Insert(13)
+		bt.root.checkElements(t, 3, 6, 9)
+		bt.root.checkChildrenLength(t, 4)
+		bt.root.children[0].checkElements(t, 1, 2)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 4, 5)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 7, 8)
+		bt.root.children[2].checkChildrenLength(t, 0)
+		bt.root.children[3].checkElements(t, 10, 11, 12, 13)
+		bt.root.children[3].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 14", func(t *testing.T) {
+		bt.Insert(14)
+		bt.root.checkElements(t, 3, 6, 9, 12)
+		bt.root.checkChildrenLength(t, 5)
+		bt.root.children[0].checkElements(t, 1, 2)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 4, 5)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 7, 8)
+		bt.root.children[2].checkChildrenLength(t, 0)
+		bt.root.children[3].checkElements(t, 10, 11)
+		bt.root.children[3].checkChildrenLength(t, 0)
+		bt.root.children[4].checkElements(t, 13, 14)
+		bt.root.children[4].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 15", func(t *testing.T) {
+		bt.Insert(15)
+		bt.root.checkElements(t, 3, 6, 9, 12)
+		bt.root.checkChildrenLength(t, 5)
+		bt.root.children[0].checkElements(t, 1, 2)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 4, 5)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 7, 8)
+		bt.root.children[2].checkChildrenLength(t, 0)
+		bt.root.children[3].checkElements(t, 10, 11)
+		bt.root.children[3].checkChildrenLength(t, 0)
+		bt.root.children[4].checkElements(t, 13, 14, 15)
+		bt.root.children[4].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 16", func(t *testing.T) {
+		bt.Insert(16)
+		bt.root.checkElements(t, 3, 6, 9, 12)
+		bt.root.checkChildrenLength(t, 5)
+		bt.root.children[0].checkElements(t, 1, 2)
+		bt.root.children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].checkElements(t, 4, 5)
+		bt.root.children[1].checkChildrenLength(t, 0)
+		bt.root.children[2].checkElements(t, 7, 8)
+		bt.root.children[2].checkChildrenLength(t, 0)
+		bt.root.children[3].checkElements(t, 10, 11)
+		bt.root.children[3].checkChildrenLength(t, 0)
+		bt.root.children[4].checkElements(t, 13, 14, 15, 16)
+		bt.root.children[4].checkChildrenLength(t, 0)
+	})
+
+	t.Run("insert 17", func(t *testing.T) {
+		bt.Insert(17)
+
+		// top level
+		bt.root.checkElements(t, 9)
+		bt.root.checkChildrenLength(t, 2)
+
+		// second level left to right
+		bt.root.children[0].checkElements(t, 3, 6)
+		bt.root.children[0].checkChildrenLength(t, 3)
+
+		bt.root.children[1].checkElements(t, 12, 15)
+		bt.root.children[1].checkChildrenLength(t, 3)
+
+		// third level left to right
+		bt.root.children[0].children[0].checkElements(t, 1, 2)
+		bt.root.children[0].children[0].checkChildrenLength(t, 0)
+		bt.root.children[0].children[1].checkElements(t, 4, 5)
+		bt.root.children[0].children[1].checkChildrenLength(t, 0)
+		bt.root.children[0].children[2].checkElements(t, 7, 8)
+		bt.root.children[0].children[2].checkChildrenLength(t, 0)
+
+		bt.root.children[1].children[0].checkElements(t, 10, 11)
+		bt.root.children[1].children[0].checkChildrenLength(t, 0)
+		bt.root.children[1].children[1].checkElements(t, 13, 14)
+		bt.root.children[1].children[1].checkChildrenLength(t, 0)
+		bt.root.children[1].children[2].checkElements(t, 16, 17)
+		bt.root.children[1].children[2].checkChildrenLength(t, 0)
+	})
+}
+
+func TestInsertDuplicates(t *testing.T) {
+	bt, _ := New(3)
+	bt.Insert(1)
+	bt.Insert(1)
+	bt.Insert(1)
+	bt.Insert(1)
+	bt.root.checkElements(t, 1)
+	bt.root.children[0].checkElements(t, 1)
+	bt.root.children[1].checkElements(t, 1, 1)
+}
+
+func TestExists(t *testing.T) {
+	degree := 3
+	bt, _ := New(degree, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
+
+	t.Run("exists", func(t *testing.T) {
+		if !bt.Exists(5) {
+			t.Error("expected 5 to exist")
+		}
+		if !bt.Exists(13) {
+			t.Error("expected 13 to exist")
+		}
+	})
+
+	t.Run("not exists", func(t *testing.T) {
+		if bt.Exists(0) {
+			t.Error("did not expect 0 to exist")
+		}
+		if bt.Exists(14) {
+			t.Error("did not expect 14 to exist")
+		}
+	})
+
+	t.Run("empty tree", func(t *testing.T) {
+		et, _ := New(3)
+		if et.Exists(1) {
+			t.Errorf("expected 1 to not exist because the tree is empty")
+		}
+	})
+}
+
+// checkElements asserts a node's elements match exactly the values for elements.
+// order does matter.
+func (n *node) checkElements(t *testing.T, elements ...int) {
+	if len(n.elements) != len(elements) {
+		t.Errorf(
+			"Got node with %v elements, but want node with %v elements",
+			len(n.elements),
+			len(elements),
+		)
+	}
+	for i, e := range elements {
+		if n.elements[i] != e {
+			t.Errorf("Invalid match %v with %v", n.elements[i], e)
+		}
+	}
+}
+
+func (n *node) checkChildrenLength(t *testing.T, expectedLength int) {
+	if len(n.children) != expectedLength {
+		t.Errorf(
+			"Got %v children, but expected %v children",
+			len(n.children),
+			expectedLength,
+		)
+	}
+}
