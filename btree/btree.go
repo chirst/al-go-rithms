8e2858ab483b88f15bcfdae8d2627ce607050765
Colton Hirst <colton141@live.com>
2022-03-17 23:27:57 -0600

    Smaller functions (#3)

--- btree/btree.go
+++ btree/btree.go
@@ -3,7 +3,7 @@ package btree
 
 // TODO:
 // - implement Delete
-// - smaller functions
+// - make comments less bad
 
 import (
 	"errors"
@@ -128,114 +128,182 @@ func (bt *btree) insert(n *node, value int) {
 // split splits a node exceeding a tree's degree. split continues to recursively
 // process parent nodes until all parent nodes have valid degrees.
 func (bt *btree) split(n *node) {
-	// Base case, done splitting.
+	// Base case.
+	// Done splitting, the current node and all parent nodes have valid degrees.
 	if len(n.elements) < bt.degree {
 		return
 	}
 
-	// `n` is the root node. This means the tree will grow in height.
+	// Base case #2.
+	// `n` is the root node and needs to be split.
 	if n.parent == nil {
-		// Gather left, right, and middle partitions of the current node.
-		middleIndex := (len(n.elements) - 1) / 2
-		value := n.elements[middleIndex]
-		lefts := n.elements[:middleIndex]
-		rights := n.elements[middleIndex+1:]
-		leftChildren := []*node{}
-		rightChildren := []*node{}
-		if len(n.children) != 0 {
-			leftChildren = n.children[:middleIndex+1]
-			rightChildren = n.children[middleIndex+1:]
-		}
-
-		// Create a new root node from the middle partition (the middle
-		// partition always being size 1) making the root node's two children
-		// from the left and right partition.
-		bt.root = &node{
-			elements: []int{value},
-		}
-		bt.root.children = append(bt.root.children, &node{
-			parent:   bt.root,
-			elements: lefts,
-			children: leftChildren,
-		})
-		bt.root.children = append(bt.root.children, &node{
-			parent:   bt.root,
-			elements: rights,
-			children: rightChildren,
-		})
+		bt.splitRoot(n)
 		return
-	} else {
-		// Gather left, right, and middle partitions of the current node.
-		middleIndex := (len(n.elements) - 1) / 2
-		middleElement := n.elements[middleIndex]
-		leftElements := n.elements[:middleIndex]
-		rightElements := n.elements[middleIndex+1:]
-		leftChildren := []*node{}
-		rightChildren := []*node{}
-		if len(n.children) != 0 {
-			rightChildren = n.children[middleIndex+1:]
-			leftChildren = n.children[:middleIndex-1]
-		}
-
-		// Remove old parent/child relation.
-		for i, element := range n.parent.elements {
-			// End of list, remove last child.
-			if element < leftElements[0] && i+1 == len(n.parent.elements) {
-				n.parent.children = n.parent.children[:len(n.parent.children)-1]
-				break
-			}
-			// Gone past by 1.
-			if element > leftElements[0] {
-				n.parent.children = append(n.parent.children[:i], n.parent.children[i+1:]...)
-				break
-			}
-		}
-
-		// Insert middle into parent and put lefts and rights as children.
-		for i, element := range n.parent.elements {
-			// End of list, append to the end.
-			if element < middleElement && i+1 == len(n.parent.elements) {
-				n.parent.elements = append(n.parent.elements, middleElement)
-				newLeft := &node{
-					parent:   n.parent,
-					elements: leftElements,
-					children: leftChildren,
-				}
-				n.parent.children = append(n.parent.children, newLeft)
-				newRight := &node{
-					parent:   n.parent,
-					elements: rightElements,
-					children: rightChildren,
-				}
-				n.parent.children = append(n.parent.children, newRight)
-			}
-			// Gone past by 1.
-			if element > middleElement {
-				// Insert middle into parent elements.
-				leftParentElements := n.parent.elements[:i-1]
-				leftParentElements = append(leftParentElements, middleElement)
-				rightParentElements := n.parent.elements[i:]
-				n.parent.elements = append(leftParentElements, rightParentElements...)
-
-				// Insert lefts and rights into parent children.
-				leftParentChildren := n.parent.children[:i-1]
-				newLeft := &node{
-					parent:   n.parent,
-					elements: leftElements,
-					children: leftChildren,
-				}
-				leftParentChildren = append(leftParentChildren, newLeft)
-				newRight := &node{
-					parent:   n.parent,
-					elements: rightElements,
-					children: rightChildren,
-				}
-				leftParentChildren = append(leftParentChildren, newRight)
-				rightParentChildren := n.parent.children[i:]
-				n.parent.children = append(leftParentChildren, rightParentChildren...)
-			}
-		}
-		// Recursively continue to split.
-		bt.split(n.parent)
 	}
+
+	// `n` is an internal node that needs splitting.
+	bt.splitInternal(n)
+
+	// Recursively continue to split.
+	bt.split(n.parent)
+}
+
+// splitRoot takes an invalid degree root node and creates a new root node with
+// the previously invalid root node evenly split as the new root nodes children.
+// This procedure is what grows the tree in height.
+func (bt *btree) splitRoot(n *node) {
+	middle, lefts, rights := getPartitionedElementsOf(n)
+	leftChildren, rightChildren := getPartitionedChildrenOf(n)
+	bt.root = &node{
+		elements: []int{middle},
+	}
+	bt.root.children = append(bt.root.children, &node{
+		parent:   bt.root,
+		elements: lefts,
+		children: leftChildren,
+	})
+	bt.root.children = append(bt.root.children, &node{
+		parent:   bt.root,
+		elements: rights,
+		children: rightChildren,
+	})
+	return
+}
+
+// splitInternal takes an internal node `n` and inserts `n`'s middle node into
+// the parent. The partitions to the left and right of `n`'s middle node then
+// become children of `n`'s parent.
+//
+// An internal node is a node who has a parent and children.
+func (bt *btree) splitInternal(n *node) {
+	middleElement, leftElements, rightElements := getPartitionedElementsOf(n)
+	leftChildren, rightChildren := getPartitionedChildrenOf(n)
+
+	removeChildFromParent(n, leftElements)
+
+	// Insert middle into parent and put lefts and rights as children.
+	for i, element := range n.parent.elements {
+		// End of list, append to the end.
+		if element < middleElement && i+1 == len(n.parent.elements) {
+			appendSplitInternal(
+				n,
+				middleElement,
+				leftElements,
+				leftChildren,
+				rightElements,
+				rightChildren,
+			)
+		}
+		// Gone past by 1 insert in order.
+		if element > middleElement {
+			insertSplitInternal(
+				n,
+				i,
+				middleElement,
+				leftElements,
+				leftChildren,
+				rightElements,
+				rightChildren,
+			)
+		}
+	}
+}
+
+// removeChildFromParent takes a node `n` and removes the relation to `n` in
+// `n`'s parent.
+func removeChildFromParent(n *node, leftElements []int) {
+	for i, element := range n.parent.elements {
+		// End of list, remove last child.
+		if i+1 == len(n.parent.elements) {
+			n.parent.children = n.parent.children[:len(n.parent.children)-1]
+			break
+		}
+		// Gone past by 1.
+		if element > leftElements[0] {
+			n.parent.children = append(n.parent.children[:i], n.parent.children[i+1:]...)
+			break
+		}
+	}
+}
+
+// appendSplitInternal appends a split internal node `n` to the end of `n`'s
+// parent `p` by appending the middle element to `p`'s elements and appending
+// new children to the end of `p`'s children.
+func appendSplitInternal(
+	n *node,
+	middleElement int,
+	leftElements []int,
+	leftChildren []*node,
+	rightElements []int,
+	rightChildren []*node,
+) {
+	n.parent.elements = append(n.parent.elements, middleElement)
+	newLeft := &node{
+		parent:   n.parent,
+		elements: leftElements,
+		children: leftChildren,
+	}
+	n.parent.children = append(n.parent.children, newLeft)
+	newRight := &node{
+		parent:   n.parent,
+		elements: rightElements,
+		children: rightChildren,
+	}
+	n.parent.children = append(n.parent.children, newRight)
+}
+
+// insertSplitInternal inserts a split internal node `n` made up of middle,
+// lefts, and rights in order with `n`'s parent's elements and children.
+func insertSplitInternal(
+	n *node,
+	i int,
+	middleElement int,
+	leftElements []int,
+	leftChildren []*node,
+	rightElements []int,
+	rightChildren []*node,
+) {
+	leftParentElements := n.parent.elements[:i-1]
+	leftParentElements = append(leftParentElements, middleElement)
+	rightParentElements := n.parent.elements[i:]
+	n.parent.elements = append(leftParentElements, rightParentElements...)
+
+	leftParentChildren := n.parent.children[:i-1]
+	newLeft := &node{
+		parent:   n.parent,
+		elements: leftElements,
+		children: leftChildren,
+	}
+	leftParentChildren = append(leftParentChildren, newLeft)
+	newRight := &node{
+		parent:   n.parent,
+		elements: rightElements,
+		children: rightChildren,
+	}
+	leftParentChildren = append(leftParentChildren, newRight)
+	rightParentChildren := n.parent.children[i:]
+	n.parent.children = append(leftParentChildren, rightParentChildren...)
+}
+
+// getPartitionedElementsOf splits and returns the middle, left, and right
+// elements of the given node.
+func getPartitionedElementsOf(n *node) (int, []int, []int) {
+	middleIndex := (len(n.elements) - 1) / 2
+	middle := n.elements[middleIndex]
+	lefts := n.elements[:middleIndex]
+	rights := n.elements[middleIndex+1:]
+	return middle, lefts, rights
+}
+
+// getPartitionedChildrenOf splits and returns the children of the given node
+// into left and right partitions.
+func getPartitionedChildrenOf(n *node) ([]*node, []*node) {
+	middleIndex := (len(n.elements) - 1) / 2
+	lefts := []*node{}
+	rights := []*node{}
+	if len(n.children) != 0 {
+		lefts = n.children[:middleIndex+1]
+		rights = n.children[middleIndex+1:]
+	}
+	return lefts, rights
 }
