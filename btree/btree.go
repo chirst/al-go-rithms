dc9558e339f20ec319de55fcf626e7127db567d3
Colton Hirst <colton141@live.com>
2022-04-12 22:36:04 -0600

    Cleanup (#4)
    Cleanup and improve comments. Fix bug where elements did not maintain
    order. Ordering of function definitions.

--- btree/btree.go
+++ btree/btree.go
@@ -2,23 +2,32 @@
 package btree
 
 // TODO:
-// - implement Delete
-// - make comments less bad
+// - Implement Delete
+// - Test coverage
 
 import (
 	"errors"
 )
 
+// btree represents a single btree data structure made up of nodes.
 type btree struct {
-	root   *node
+	// root is the entry node of the tree.
+	root *node
+	// degree is the maximum amount of elements a node in the btree can contain.
+	// When the maximum is exceeded the node will perform a split operation.
 	degree int
 }
 
-// node consists of one or many elements and many children and always maintains
-// elements + 1 number of children.
+// node makes up a btree. There are three different kinds of nodes in this tree:
+// Root, a node with no parent; Internal, a node with a parent and children;
+// Leaf, a node with a parent and no children.
 type node struct {
-	parent   *node
+	// parent is nil when the node is the root of the tree.
+	parent *node
+	// elements are ordered from least to greatest. elements do not exceed the
+	// degree of their associated btree.
 	elements []int
+	// A node maintains elements + 1 children at all times.
 	children []*node
 }
 
@@ -28,8 +37,8 @@ type node struct {
 //
 // This implementation allows degrees between 3-7 inclusive.
 //
-// Given `values` are provided the tree will be populated with the `values`. The
-// complexity of this is O(n log n).
+// Given values are provided, the tree will be populated with the values. The
+// complexity of inserting these values is O(n log n).
 func New(degree int, values ...int) (*btree, error) {
 	if degree < 3 {
 		return nil, errors.New("tree must not have degree less than 3")
@@ -46,7 +55,7 @@ func New(degree int, values ...int) (*btree, error) {
 	return nt, nil
 }
 
-// Exists checks for the existense of the given `value`.
+// Exists checks for the existence of the given value.
 //
 // The complexity is O(log n).
 func (bt *btree) Exists(value int) bool {
@@ -63,28 +72,20 @@ func (bt *btree) exists(n *node, value int) bool {
 			return true
 		}
 	}
-	// If the node is a leaf the value does not exist
+	// If the node is a leaf the value does not exist.
 	if len(n.children) == 0 {
 		return false
 	}
-	// Recursively search next node
-	var childNode *node
-	for i, el := range n.elements {
-		if el > value {
-			childNode = n.children[i]
-			break
-		}
-		if i == len(n.elements)-1 {
-			childNode = n.children[i+1]
-			break
-		}
-	}
+	// Existence is still unknown, determine what child node to search next,
+	// then recursively search the next child node.
+	childNode := n.getChildContaining(value)
 	return bt.exists(childNode, value)
 }
 
 // Insert inserts an element into the tree.
 //
-// Given the value already exists in the tree, the value will still be inserted.
+// Given the value already exists in the tree, the value will still be inserted
+// as a duplicate.
 //
 // The complexity is O(log n).
 func (bt *btree) Insert(value int) {
@@ -96,64 +97,56 @@ func (bt *btree) Insert(value int) {
 		return
 	}
 	// Root node exists, attempt to insert into the root node. In case the root
-	// node is not a leaf, `insert` will recursively find a leaf node to insert
+	// node is not a leaf, insert will recursively find a leaf node to insert
 	// into.
 	bt.insert(bt.root, value)
 }
 
 // insert recursively follows nodes until hitting a leaf node. Once a leaf is
-// hit, an insert will be performed (whether or not the insert is allowed)
-// followed by splitting to ensure the tree is in a valid state.
+// hit, an insert will be performed (whether or not the insert is allowed based
+// off of the btree's degree). Given the insert leaves the node in an invalid
+// state, splitting is performed to make the tree valid again.
 func (bt *btree) insert(n *node, value int) {
 	// Insert if leaf.
 	if len(n.children) == 0 {
-		n.elements = append(n.elements, value)
+		n.addElement(value)
 		// Start splitting if needed.
 		bt.split(n)
 	} else {
 		// Recursively try to insert on the next sub tree.
-		for i, el := range n.elements {
-			if el > value {
-				bt.insert(n.children[i], value)
-				break
-			}
-			if i == len(n.elements)-1 {
-				bt.insert(n.children[i+1], value)
-				break
-			}
-		}
+		childNode := n.getChildContaining(value)
+		bt.insert(childNode, value)
 	}
 }
 
 // split splits a node exceeding a tree's degree. split continues to recursively
 // process parent nodes until all parent nodes have valid degrees.
 func (bt *btree) split(n *node) {
-	// Base case.
-	// Done splitting, the current node and all parent nodes have valid degrees.
+	// Done splitting, the current node is a valid degree.
 	if len(n.elements) < bt.degree {
 		return
 	}
 
-	// Base case #2.
-	// `n` is the root node and needs to be split.
+	// The current node is the root node and needs to be split.
 	if n.parent == nil {
-		bt.splitRoot(n)
+		bt.splitRoot()
 		return
 	}
 
-	// `n` is an internal node that needs splitting.
+	// The current node is an internal node that needs splitting.
 	bt.splitInternal(n)
 
 	// Recursively continue to split.
 	bt.split(n.parent)
 }
 
-// splitRoot takes an invalid degree root node and creates a new root node with
-// the previously invalid root node evenly split as the new root nodes children.
+// splitRoot creates a new root node with the middle element of the root node as
+// the new root node. The left and right partitions become the new root nodes
+// children.
 // This procedure is what grows the tree in height.
-func (bt *btree) splitRoot(n *node) {
-	middle, lefts, rights := getPartitionedElementsOf(n)
-	leftChildren, rightChildren := getPartitionedChildrenOf(n)
+func (bt *btree) splitRoot() {
+	middle, lefts, rights := bt.root.getPartitionedElements()
+	leftChildren, rightChildren := bt.root.getPartitionedChildren()
 	bt.root = &node{
 		elements: []int{middle},
 	}
@@ -170,23 +163,20 @@ func (bt *btree) splitRoot(n *node) {
 	return
 }
 
-// splitInternal takes an internal node `n` and inserts `n`'s middle node into
-// the parent. The partitions to the left and right of `n`'s middle node then
-// become children of `n`'s parent.
-//
-// An internal node is a node who has a parent and children.
+// splitInternal takes an internal node and inserts it's middle element into the
+// parent. The partitions to the left and right of the middle element then
+// become children of the parent.
 func (bt *btree) splitInternal(n *node) {
-	middleElement, leftElements, rightElements := getPartitionedElementsOf(n)
-	leftChildren, rightChildren := getPartitionedChildrenOf(n)
+	middleElement, leftElements, rightElements := n.getPartitionedElements()
+	leftChildren, rightChildren := n.getPartitionedChildren()
 
-	removeChildFromParent(n, leftElements)
+	n.removeChildFromParent(leftElements)
 
 	// Insert middle into parent and put lefts and rights as children.
 	for i, element := range n.parent.elements {
 		// End of list, append to the end.
 		if element < middleElement && i+1 == len(n.parent.elements) {
-			appendSplitInternal(
-				n,
+			n.appendSplitInternal(
 				middleElement,
 				leftElements,
 				leftChildren,
@@ -196,8 +186,7 @@ func (bt *btree) splitInternal(n *node) {
 		}
 		// Gone past by 1 insert in order.
 		if element > middleElement {
-			insertSplitInternal(
-				n,
+			n.insertSplitInternal(
 				i,
 				middleElement,
 				leftElements,
@@ -209,9 +198,55 @@ func (bt *btree) splitInternal(n *node) {
 	}
 }
 
-// removeChildFromParent takes a node `n` and removes the relation to `n` in
-// `n`'s parent.
-func removeChildFromParent(n *node, leftElements []int) {
+// addElement adds an element to a leaf node while maintaining ordering of the
+// elements.
+func (n *node) addElement(value int) {
+	for i, e := range n.elements {
+		if value < e {
+			n.elements = append(n.elements[:i+1], n.elements[i:]...)
+			n.elements[i] = value
+			return
+		}
+	}
+	n.elements = append(n.elements, value)
+}
+
+// getChildContaining returns the child node potentially containing the given
+// value.
+func (n *node) getChildContaining(value int) *node {
+	for i, el := range n.elements {
+		if el > value {
+			return n.children[i]
+		}
+	}
+	return n.children[len(n.children)-1]
+}
+
+// getPartitionedElements splits and returns the middle, left, and right
+// elements of the given node.
+func (n *node) getPartitionedElements() (int, []int, []int) {
+	middleIndex := (len(n.elements) - 1) / 2
+	middle := n.elements[middleIndex]
+	lefts := n.elements[:middleIndex]
+	rights := n.elements[middleIndex+1:]
+	return middle, lefts, rights
+}
+
+// getPartitionedChildren splits and returns the children of the given node
+// into left and right partitions.
+func (n *node) getPartitionedChildren() ([]*node, []*node) {
+	middleIndex := (len(n.elements) - 1) / 2
+	lefts := []*node{}
+	rights := []*node{}
+	if len(n.children) != 0 {
+		lefts = n.children[:middleIndex+1]
+		rights = n.children[middleIndex+1:]
+	}
+	return lefts, rights
+}
+
+// removeChildFromParent removes the relation between the node and it's parent.
+func (n *node) removeChildFromParent(leftElements []int) {
 	for i, element := range n.parent.elements {
 		// End of list, remove last child.
 		if i+1 == len(n.parent.elements) {
@@ -226,11 +261,9 @@ func removeChildFromParent(n *node, leftElements []int) {
 	}
 }
 
-// appendSplitInternal appends a split internal node `n` to the end of `n`'s
-// parent `p` by appending the middle element to `p`'s elements and appending
-// new children to the end of `p`'s children.
-func appendSplitInternal(
-	n *node,
+// appendSplitInternal appends a split internal node to the end of the nodes
+// parent by appending the split partitions to the nodes parent.
+func (n *node) appendSplitInternal(
 	middleElement int,
 	leftElements []int,
 	leftChildren []*node,
@@ -252,10 +285,9 @@ func appendSplitInternal(
 	n.parent.children = append(n.parent.children, newRight)
 }
 
-// insertSplitInternal inserts a split internal node `n` made up of middle,
-// lefts, and rights in order with `n`'s parent's elements and children.
-func insertSplitInternal(
-	n *node,
+// insertSplitInternal inserts a split internal node made up of middle,
+// lefts, and rights in order with the parent node elements and children.
+func (n *node) insertSplitInternal(
 	i int,
 	middleElement int,
 	leftElements []int,
@@ -283,27 +315,4 @@ func insertSplitInternal(
 	leftParentChildren = append(leftParentChildren, newRight)
 	rightParentChildren := n.parent.children[i:]
 	n.parent.children = append(leftParentChildren, rightParentChildren...)
-}
-
-// getPartitionedElementsOf splits and returns the middle, left, and right
-// elements of the given node.
-func getPartitionedElementsOf(n *node) (int, []int, []int) {
-	middleIndex := (len(n.elements) - 1) / 2
-	middle := n.elements[middleIndex]
-	lefts := n.elements[:middleIndex]
-	rights := n.elements[middleIndex+1:]
-	return middle, lefts, rights
-}
-
-// getPartitionedChildrenOf splits and returns the children of the given node
-// into left and right partitions.
-func getPartitionedChildrenOf(n *node) ([]*node, []*node) {
-	middleIndex := (len(n.elements) - 1) / 2
-	lefts := []*node{}
-	rights := []*node{}
-	if len(n.children) != 0 {
-		lefts = n.children[:middleIndex+1]
-		rights = n.children[middleIndex+1:]
-	}
-	return lefts, rights
 }
